441131eb41ddfd1c68c2b83b55eb07baa39211fe
NehaJadhavTakawane <neha@birdvision.in>
2021-11-25 20:28:23 +0530

    test

---
+++ Sara.java
@@ -0,0 +1,20 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sara {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver","D:\\chrome\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.co.in/n");
+		driver.close();
+
+}
+
+}
+
+
+
